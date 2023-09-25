b07644bebc5acb4712f738d32e1be32ece85efd4
Ghelder <Jhelder@outlook.com>
2023-09-24 23:38:02 -0500

    feat: prints the title of a movie

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+
+function getMovieTitle(movieId) {
+  const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const movie = JSON.parse(body);
+      console.log(`${movie.title}`);
+    }
+  });
+}
+
+const movieId = process.argv[2];
+getMovieTitle(movieId);
