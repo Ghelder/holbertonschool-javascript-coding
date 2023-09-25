3b9402da7740159f82a0ed1129ed5c9562e23d4c
Ghelder <Jhelder@outlook.com>
2023-09-24 23:05:31 -0500

    fix: rules semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-function readFile(filePath) {
-  fs.readFile(filePath, "utf-8", (err, data) => {
+function readFile (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
       console.error(err);
       return;
